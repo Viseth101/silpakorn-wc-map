6e29e884906992fb2a1412ad70a572e27f087fc4
sotheasokea <sotheasokea7@gmail.com>
2026-03-06 22:12:33 +0700

    add the node_modules and update the server.js, wcList.json files

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,16 @@
-const detail = []
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/wc', (req, res) =>{
+  const data = fs.readFileSync('wcList.json', 'utf-8');
+  res.json(JSON.parse(data));
+})
+
+app.listen(3000, ()=>{
+  console.log('Server is running');
+})
